29615c003f9c2fd26f0c6696b5260b328ada28d0
Panji Muttaqin <panji.muttaqin@me.com>
2020-04-25 14:15:26 +0700

    alsus validasi

--- validator/alsus.js
+++ validator/alsus.js
@@ -1,14 +1,16 @@
 const Joi = require('@hapi/joi')
 const schema = Joi.object({
-    tahun_penggunaan_alsus: Joi.number().min(2000).required()
+    tahun_penggunaan_alsus: Joi.number().min(2010).required()
     .messages({ 
         "number.min": "Format tahun salah", 
-        "any.required": "Tahun harus diisi"
+        "any.required": "Tahun harus diisi",
+        "number.base": "Format tahun salah",
       }),
     jumlah_penggunaan_alsus: Joi.number().min(1).required()
     .messages({ 
         "number.min": "Format jumlah salah", 
-        "any.required": "Jumlah harus diisi"
+        "any.required": "Jumlah harus diisi",
+        "number.base": "Format jumlah salah",
       }),
 });
  
